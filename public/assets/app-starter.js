b529ce98fef842363eb49b3217e3ab7bbcc18dae
Christopher DiStasio <cadistasio@gmail.com>
2023-02-08 14:21:06 -0600

    Adds starter code file

--- public/assets/app-starter.js
+++ public/assets/app-starter.js
@@ -1,11 +1,17 @@
 // create map
+// Create map:
+
 
 
 // add openstreetmap tiles
 
+// Add OpenStreetMap tiles:
+
 
 // create and main add geolocation marker
 
+
+// Create and add a geolocation marker:
 
 // draw the 2nd arrondissement
 
@@ -14,6 +20,3 @@
 
 
 // metro station markers
-
-
-
